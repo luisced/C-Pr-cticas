6aae4abbbfb48e728ddf4ce2953576ac9925aa30
Luis Cedillo Maldonado <luis.cedillo.maldonado@gmail.com>
2022-09-15 08:36:32 -0500

    Ejercicio de números armstrong entre dos números

--- C#/ejercicios/betweenNumbersArmstrong/Program.cs
+++ C#/ejercicios/betweenNumbersArmstrong/Program.cs
@@ -4,17 +4,26 @@
     {
         static void Main(string[] args)
         {
-            int n1, n2;
-            Console.WriteLine("Ingrese un numero: ");
+            int n1, n2, n3;
+            Console.Write("Ingrese un número: ");
+            n3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese un limite1: ");
             n1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Ingrese otro numero: ");
+            Console.Write("Ingrese limite2: ");
             n2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Los números armstrong entre: {n1} y {n2} son: ");
+
+            if (Armstrong(n3))
+
+                Console.WriteLine($"{n3} es un número armstrong");
+            else
+                Console.WriteLine("No es un número armstrong");
+
             // print the all the numbers between n1 and n2
             for (int i = n1; i <= n2; i++)
             {
                 //   know if the number is armstrong or not
-                if (isArmstrong(i))
+                if (Armstrong(i))
                 {
                     Console.Write($"{i}, ");
                 }
@@ -23,7 +32,7 @@
 
         }
         // function to know if the number is armstrong or not
-        static bool isArmstrong(int n)
+        static bool Armstrong(int n)
         {
             int sum = 0, temp = n, digits = 0;
             // count the number of digits
